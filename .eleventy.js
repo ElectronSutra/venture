228a54d3c04d2f0638f5cb9f093d9a071d271327
ElectronSutra <em@email.com>
2025-01-11 02:34:35 -0500

    Try a different build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
   return {
     dir: { input: "pages", output: "_site" },
+    pathPrefix: "/venture/",
   };
 };
